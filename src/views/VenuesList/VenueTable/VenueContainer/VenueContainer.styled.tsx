a8325f4831539e3e6a5429945c488c0745c1fe97
NameIsFill <filpopowski@gmail.com>
2023-10-20 21:04:31 +0200

    forwarding props to styled component

--- src/views/VenuesList/VenueTable/VenueContainer/VenueContainer.styled.tsx
+++ src/views/VenuesList/VenueTable/VenueContainer/VenueContainer.styled.tsx
@@ -1,12 +1,16 @@
 import { styled } from "@mui/material/styles";
-import {FC} from "react";
 
-type ContainerProps = {
+type InputProps = {
     background: number,
 }
-export const StyledVenueContainer: FC<ContainerProps> = styled('div')`
-  width: 282px;
-  height: 228px;
-  background-image: url("https://picsum.photos/id/${(props:string) => props.background}/282/228");
-  margin: 10px 0px;
-`;
+
+export const StyledVenueContainer = styled('div')<InputProps>(({ background }) => ({
+    background-image: url("https://picsum.photos/id/${background}/282/228")
+}));
+
+// export const StyledVenueContainer: FC<ContainerProps> = styled('div')`
+//   width: 282px;
+//   height: 228px;
+//   background-image: url("https://picsum.photos/id/${(props:string) => props.background}/282/228");
+//   margin: 10px 0px;
+// `;
